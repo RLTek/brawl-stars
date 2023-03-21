910e6c091814b1504e96792c9250f4b10e2dd462
RL Tek <Rlusby@live.com>
2023-03-20 20:42:40 -0400

    added logo to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,7 @@
 import {Link} from 'react-router-dom'
 
 import hero from '../images/brawlStars.jpg'
+import logo from '../images/logo.png'
 
 
 import './navbar.css'
@@ -12,7 +13,8 @@ export default function Navbar() {
     return(
         <header id="navbar-page">
             <img src={hero} alt="Brawl Stars" id="hero"/>
-            <h1><span id="brawl">Brawl</span> <span id="stars">Stars</span></h1>
+            <h1><span id="brawl">Brawl</span> <span id="stars">Stars <img src={logo} alt="logo" id="logo"/></span></h1>
+            
         <ul id ="navbar">
             <Link to="/"><li>Home</li></Link>
             <Link to="brawlers"><li>Brawlers</li></Link>
